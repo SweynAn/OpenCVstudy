d4291d6f34ad8b490cc1ba143f7f56f759a15575
SweynAn <sha020@ucsd.edu>
2018-09-13 15:29:24 -0700

    First Commit for read in pictures and videos

---
+++ project2/main.cpp
@@ -0,0 +1,22 @@
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+
+using namespace cv;
+using namespace std;
+
+int main( int argc, char** argv ) {
+  cv::namedWindow( "Example3", cv::WINDOW_AUTOSIZE );
+  cv::VideoCapture cap;
+  cap.open( std::string(argv[1]) );
+  cv::Mat frame;
+  for(;;) {
+    cap >> frame;
+    if( frame.empty() ) break;
+    cv::imshow( "Example3", frame );
+    if( cv::waitKey(33) >= 0 ) break;
+}
+return 0; }
